17662304793824830fdb6b08fd20dfb6812129b4
Hung Tran <tranvuhung06@gmail.com>
2019-12-19 15:29:08 +0700

    fix android platform

--- android/src/main/java/com/example/izeta_flutter_lottie/IzetaFlutterLottiePlugin.java
+++ android/src/main/java/com/example/izeta_flutter_lottie/IzetaFlutterLottiePlugin.java
@@ -7,25 +7,29 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** IzetaFlutterLottiePlugin */
-public class IzetaFlutterLottiePlugin { //implements MethodCallHandler
+public class IzetaFlutterLottiePlugin implements MethodCallHandler {
   /** Plugin registration. */
-  // public static void registerWith(Registrar registrar) {
-  //   final MethodChannel channel = new MethodChannel(registrar.messenger(), "izeta_flutter_lottie");
-  //   channel.setMethodCallHandler(new IzetaFlutterLottiePlugin());
-  // }
-
-  // @Override
-  // public void onMethodCall(MethodCall call, Result result) {
-  //   if (call.method.equals("getPlatformVersion")) {
-  //     result.success("Android " + android.os.Build.VERSION.RELEASE);
-  //   } else {
-  //     result.notImplemented();
-  //   }
-  // }
   public static void registerWith(Registrar registrar) {
+    // final MethodChannel channel = new MethodChannel(registrar.messenger(), "izeta_flutter_lottie");
+    // channel.setMethodCallHandler(new IzetaFlutterLottiePlugin());
     registrar.platformViewRegistry().registerViewFactory(
       "izeta/flutter_lottie",
       new LottieViewFactory(registrar)
     );
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
     }
+  }
+  // public static void registerWith(Registrar registrar) {
+  //   registrar.platformViewRegistry().registerViewFactory(
+  //     "izeta/flutter_lottie",
+  //     new LottieViewFactory(registrar)
+  //   );
+  //   }
 }
